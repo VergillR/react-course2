7566ac883622dcffc0d0a2dd3a085f6eda29f9b7
Vergill <vr@bowser.com>
2018-05-31 16:40:52 +0200

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,52 @@
 // entry -> output
 const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      { test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/ },
-      { test: /\.s?css$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: {
-      rewrites: [
+module.exports = (env, argv) => {
+  const isProduction = env === 'production'
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        { test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/ },
+        { test: /\.s?css$/,
+          use: [ MiniCssExtractPlugin.loader, { loader: 'css-loader', options: { sourceMap: true } }, { loader: 'sass-loader', options: { sourceMap: true } } ]
+        }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: {
+        rewrites: [
         { from: /.*bundle.js/, to: '/bundle.js' },
         { from: /.*favicon.png/, to: '/images/favicon.png' }
+        ]
+      }
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      })
+    ],
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true // set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
       ]
     }
   }
